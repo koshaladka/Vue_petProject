7417524fe8c76a8769a9ea52b629f4b7b236ef75
Alena home <79262812867@ya.ru>
2022-09-11 19:11:23 +0300

    step 1

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,48 @@
 <template>
-    <div>
-        <div class="post">
-            <div><strong>Название:</strong>Пост о JavaScript</div>
-            <div><strong>Описание:</strong>JavaScript универсальный язык программирования</div>
-        </div>
-        <div class="post">
-            <div><strong>Название:</strong>Пост о JavaScript</div>
-            <div><strong>Описание:</strong>JavaScript универсальный язык программирования</div>
-        </div>
-        <div class="post">
-            <div><strong>Название:</strong>Пост о JavaScript</div>
-            <div><strong>Описание:</strong>JavaScript универсальный язык программирования</div>
-        </div>
+    <div class="app">
+        <PostForm/>
+        <PostList :posts="posts"/>
+        
       
     </div>
 </template>
 
 <script>
+    import PostForm from "@/components/PostForm.vue"
+    import PostList from "@/components/PostList.vue"
+
+
     export default {
-        data() {
-            return {
-                likes: 0,
-                dislikes: 5,
-            }
-        },
+        components: {
+            PostForm, PostList
+    },
+       
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'Javascript', body: 'Описание поста'},
+                {id: 2, title: 'Javascript', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript', body: 'Описание поста 3'},
+                {id: 4, title: 'Javascript', body: 'Описание поста 4'},
+            ],
+            title: '',
+            body: '',
+        }
+},
 
         methods: {
-            addLike() {
-                this.likes += 1;
+            createPost () {
+                const newPost = {
+                    id: Date.now(),
+                    title: this.title,
+                    body: this.body,
+                }
+                this.posts.push(newPost);
+                this.title = '';
+                this.body = '';
             },
-            disLike() {
-                this.dislikes += 1;
-            },
+
+            
         }
     }
 
@@ -44,10 +55,12 @@
         box-sizing: border-box; 
     }
 
-    .post {
-        margin-top: 15px;
-        padding: 15px;
-        border: 2px solid teal;
+    .app {
+        padding: 20px;
     }
+
+    
+
+   
 
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,65 @@
+<template>
+    <form @submit.prevent>
+            <h4 class="title">Создание поста</h4>
+            <input 
+                v-bind:value="title" 
+                @input="title = $event.target.value"
+                class="input" 
+                type="text" 
+                placeholder="Название">
+            <input 
+                v-bind:value="body"
+                @input="body = $event.target.value" 
+                class="input" 
+                type="text" 
+                placeholder="Описание">
+            <button 
+                
+                class="btn">
+                Создать
+            </button>
+
+        </form>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+               
+            }
+        }
+    }
+
+</script>
+
+
+
+<style>
+     .title {
+        font-size: 20px;
+        color: teal;
+        font-weight: 600;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+
+    }
+    .input {
+        width: 100%;
+        border: 1px solid teal;
+        padding:  10px 15px;
+        margin-top: 10px;
+    }
+    .btn {
+        margin-top: 15px;
+        align-self: flex-end;
+        padding: 10px 15px;
+        background: none;
+        color: teal;
+        border: 1px solid teal;
+    }
+</style>
